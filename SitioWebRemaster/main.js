d659598338246346280f0e69c8cf1e4ac7ba8fd1
Christian Díaz <christiandp99@gmail.com>
2024-07-27 14:53:16 -0300

    Actualización

--- SitioWebRemaster/main.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.menu-btn').addEventListener('click', () => {     //al pulsar el boton 
-    document.querySelector('.nav-menu').classList.toggle('despliegue');   //añadimos o quitamos, dependiendo del estado anterior, la case despliegue al nav-menu
-})
-
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.news-cards', { delay: 500});
-ScrollReveal().reveal('.cards-banner-one', { delay: 500});
-ScrollReveal().reveal('.cards-banner-two', { delay: 500});
