4a06ca677b9b0fa0c3c80b60432e6277ae183eae
Christian Díaz <christiandp99@gmail.com>
2024-08-11 23:37:06 -0300

    Añado modal ejemplo eplan a página de habilidades

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', { delay: 500});
 ScrollReveal().reveal('.cards-banner-one', { delay: 500});
 ScrollReveal().reveal('.cards-banner-two', { delay: 500});
+
+// Modales 
+const btnAbrirModal = 
+document.querySelector("#id-btn-abrir-modal");
+const btnCerrarModal =
+document.querySelector("#id-btn-cerrar-modal");
+const modal =
+document.querySelector("#id-modal");
+
+btnAbrirModal.addEventListener("click",()=>{
+    modal.showModal();
+})
+btnCerrarModal.addEventListener("click",()=>{
+    modal.close();
+})
